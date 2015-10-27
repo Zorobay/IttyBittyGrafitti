8464e9667692719545c28e0c7ead078a7c63ffa1
Sebastian Hegardt <se.hegardt@gmail.com>
2015-10-27 23:57:02 +0100

    Sebbe desktop ändrar

--- src/userInterface/UI.java
+++ src/userInterface/UI.java
@@ -3,6 +3,6 @@ package userInterface;
 public class UI {
 
 	public static void main(String[] args) {
-		System.out.println("Sebbes dekstop ändrar");
+		System.out.println("Sebbes DESKTOP ändrar");
 	}
 }
