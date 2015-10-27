31354d8925f8119facbd7c7b2d4a918d16e6e702
Sebastian Hegardt <se.hegardt@gmail.com>
2015-10-27 22:12:10 +0100

    Verified that the project is set up and working as expected!

--- src/Interface/UI.java
+++ src/Interface/UI.java
@@ -4,7 +4,7 @@ public class UI {
 
 	public static void main(String[] args) {
 		System.out.println("Shit my bit nu ska här kodas!");
-
+		System.out.println("Sebastians laptop har också fått det att fungera!!!");
 	}
 
 }
