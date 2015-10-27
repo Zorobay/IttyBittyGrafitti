5f776553185035a17a1e98f573e55fd5cb096185
Sebastian Hegardt <se.hegardt@gmail.com>
2015-10-27 23:04:38 +0100

    Sebbe laptop makes a change. I'm testing merge conflicts...

--- src/Interface/UI.java
+++ src/Interface/UI.java
@@ -3,8 +3,7 @@ package Interface;
 public class UI {
 
 	public static void main(String[] args) {
-		System.out.println("Shit my bit nu ska här kodas!");
-		System.out.println("Sebastians laptop har också fått det att fungera!!!");
+		System.out.println("Sebbes laptop ändrar");
 	}
 
 }
