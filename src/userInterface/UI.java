d0c8f3ce110d1da3374e055a3a2b646373fcd28f
Sebastian Hegardt <se.hegardt@gmail.com>
2015-10-29 15:04:26 +0100

    Initiated user interface
    Simply defined the skeleton of our program. It is not runnable but only
    contains some basic controls.

--- src/userInterface/UI.java
+++ src/userInterface/UI.java
@@ -1,9 +1,36 @@
 package userInterface;
 
-public class UI {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class UI extends Application {
 	
-	//TODO: Lära sig gits
 	public static void main(String[] args) {
-		System.out.println("Hjalmar är bäst!");
+		launch(args);
+	}
+
+	
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Itty Bitty Grafitty");
+		GridPane pane = new GridPane();
+		pane.setHgap(10);
+		pane.setVgap(10);
+		pane.setAlignment(Pos.TOP_LEFT);
+		pane.setGridLinesVisible(true);
+		
+		VBox vbox = new VBox();
+		Button b = new Button("SEbbe");
+		Button b2 = new Button("Hjalmar");
+		vbox.getChildren().addAll(b, b2);
+		pane.add(vbox, 4, 4);
+		
+		Scene scene = new Scene(pane);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
