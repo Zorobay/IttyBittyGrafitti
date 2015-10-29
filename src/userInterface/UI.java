f8c5a37f88e32926c9fb6e2e84ee07301bfe8a07
Nicrophorus <hjalmar.danielson@gmail.com>
2015-10-29 13:52:53 +0100

    Change Test
    Hjalmar är bäst. Så e de.

--- src/userInterface/UI.java
+++ src/userInterface/UI.java
@@ -4,6 +4,6 @@ public class UI {
 	
 	//TODO: Lära sig gits
 	public static void main(String[] args) {
-		System.out.println("Sebbes LAPTOP ändrar");
+		System.out.println("Hjalmar är bäst!");
 	}
 }
