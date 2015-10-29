0b9cd697c0ac92ccb8b498855f8e603556ecee46
Nicrophorus <hjalmar.danielson@gmail.com>
2015-10-29 15:23:22 +0100

    Simple optimization implementation complentation introductory ching chong changes
    Hellu

--- src/userInterface/UI.java
+++ src/userInterface/UI.java
@@ -1,11 +1,14 @@
 package userInterface;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class UI extends Application {
@@ -17,17 +20,25 @@ public class UI extends Application {
 	
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Itty Bitty Grafitty");
+		
 		GridPane pane = new GridPane();
 		pane.setHgap(10);
 		pane.setVgap(10);
 		pane.setAlignment(Pos.TOP_LEFT);
 		pane.setGridLinesVisible(true);
+		pane.setPadding(new Insets(25, 25, 25, 25));
 		
-		VBox vbox = new VBox();
-		Button b = new Button("SEbbe");
-		Button b2 = new Button("Hjalmar");
-		vbox.getChildren().addAll(b, b2);
-		pane.add(vbox, 4, 4);
+		Canvas canvas = new Canvas();
+		pane.add(canvas, 0, 1);
+		canvas.setHeight(500);
+		canvas.setWidth(600);
+		canvas.getGraphicsContext2D().setFill(Color.WHITE);
+		canvas.getGraphicsContext2D().fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		
+		HBox hbox = new HBox();
+		Button b = new Button("Brush");
+		hbox.getChildren().addAll(b);
+		pane.add(hbox, 0, 0);
 		
 		Scene scene = new Scene(pane);
 		primaryStage.setScene(scene);
