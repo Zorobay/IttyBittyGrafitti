b2305db6621b4a4ed88db118dcda2539d8530a48
Nicrophorus <hjalmar.danielson@gmail.com>
2015-10-29 14:02:08 +0100

    Merge Test

--- src/userInterface/UI.java
+++ src/userInterface/UI.java
@@ -4,6 +4,6 @@ public class UI {
 	
 	//TODO: Lära sig gits
 	public static void main(String[] args) {
-		System.out.println("Hjalmar är bäst!");
+		System.out.println("Lilla Ludde är bäst!");
 	}
 }
