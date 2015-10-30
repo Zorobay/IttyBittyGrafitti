aeecb1312b3313c185c2703777d8e10eab429004
Sebastian Hegardt <se.hegardt@gmail.com>
2015-10-30 11:05:33 +0100

    Added event handler to canvas to enable painting
    The painting algorithm is very simple and crude, but will work for now
    but should be improved later. (To paint, hold left mouse button down and
    drag over canvas)

---
+++ src/canvas/IBGCanvas.java
@@ -0,0 +1,9 @@
+package canvas;
+
+import javafx.scene.canvas.Canvas;
+
+//This class will be used to define our own customized canvas.
+//TODO Come up with a better name
+public class IBGCanvas extends Canvas{
+	
+}
--- src/userInterface/UI.java
+++ src/userInterface/UI.java
@@ -1,11 +1,13 @@
 package userInterface;
 
+import canvas.IBGCanvas;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -13,35 +15,49 @@ import javafx.stage.Stage;
 
 public class UI extends Application {
 	
+	private IBGCanvas canvas = new IBGCanvas();
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
-	
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Itty Bitty Grafitty");
-		
+
+		// set up grid pane
 		GridPane pane = new GridPane();
 		pane.setHgap(10);
 		pane.setVgap(10);
 		pane.setAlignment(Pos.TOP_LEFT);
 		pane.setGridLinesVisible(true);
 		pane.setPadding(new Insets(25, 25, 25, 25));
-		
-		Canvas canvas = new Canvas();
+
+		// set up canvas to draw on
 		pane.add(canvas, 0, 1);
 		canvas.setHeight(500);
 		canvas.setWidth(600);
 		canvas.getGraphicsContext2D().setFill(Color.WHITE);
 		canvas.getGraphicsContext2D().fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 		
+		//set up test image
+		//Image img = new Image()
+		// Canvas event
+		canvas.getGraphicsContext2D().setFill(Color.BLANCHEDALMOND);
+		canvas.setOnMouseDragged(mouseDraggedCanvas);
+		
 		HBox hbox = new HBox();
 		Button b = new Button("Brush");
 		hbox.getChildren().addAll(b);
 		pane.add(hbox, 0, 0);
-		
+
 		Scene scene = new Scene(pane);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
+	
+	//TODO improve paint algorithm
+	private final EventHandler<MouseEvent> mouseDraggedCanvas = event -> {
+		canvas.getGraphicsContext2D().setFill(Color.CORNFLOWERBLUE);
+		canvas.getGraphicsContext2D().fillOval(event.getX(), event.getY(), 5, 5);
+	};
 }
