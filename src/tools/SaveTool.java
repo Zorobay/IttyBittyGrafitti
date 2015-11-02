2ad77a92677cf42a58c7bdfa2bb80e06a0eae20f
Hannes Åström <kantarelltroll@gmail.com>
2015-11-02 15:03:34 +0100

    Add savetool option for future use

---
+++ src/tools/SaveTool.java
@@ -0,0 +1,11 @@
+package tools;
+
+import javafx.scene.canvas.Canvas;
+
+public class SaveTool extends Tool{
+	public static void saveCanvas(Canvas canvas){
+		int width = (int)canvas.getWidth();
+		int height = (int)canvas.getHeight();
+		
+	}
+}
