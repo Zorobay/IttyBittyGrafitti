efebd2ce4b507c9dd10653b328e33fa163119d61
Hannes Åström <kantarelltroll@gmail.com>
2015-11-10 13:45:22 +0100

    Continue to implement SaveTool

--- src/tools/SaveTool.java
+++ src/tools/SaveTool.java
@@ -1,11 +1,15 @@
 package tools;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
 import javafx.scene.canvas.Canvas;
 
 public class SaveTool extends Tool{
-	public static void saveCanvas(Canvas canvas){
+	public static void saveCanvas(Canvas canvas, String path){
 		int width = (int)canvas.getWidth();
 		int height = (int)canvas.getHeight();
-		canvas.ge
+		BufferedImage image = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+		Graphics g = image.getGraphics();
 	}
 }
